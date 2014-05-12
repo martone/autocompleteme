f8507c2748a2204ea32a11807f349a1a23af1be0
Andy Martone <amartone@gmail.com>
2014-05-12 12:40:38 -0700

    Add top level comment.

--- autocompleteme.js
+++ autocompleteme.js
@@ -1,3 +1,9 @@
+/**
+ * autocompleteme.js
+ * Chrome extension to remove autocomplete=off attributes from all elements on the page.
+ * https://github.com/martone/autocompleteme
+ * Andy Martone (amartone@gmail.com)
+ */
 (function() {
   var elements = document.querySelectorAll('input[autocomplete=off]');
   if (elements.length) {
