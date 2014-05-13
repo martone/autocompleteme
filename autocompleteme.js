9cf5d403820e1b227ea0f795ccd82847408db480
Andy Martone <amartone@gmail.com>
2014-05-12 19:30:04 -0700

    Tweak logging.

--- autocompleteme.js
+++ autocompleteme.js
@@ -1,18 +1,20 @@
 /**
  * autocompleteme.js
  * Chrome extension to remove autocomplete=off attributes from all elements on the page.
+ *
  * https://github.com/martone/autocompleteme
  * Andy Martone (amartone@gmail.com)
  */
 (function() {
   var elements = document.querySelectorAll('input[autocomplete=off]');
   if (elements.length) {
-    console.group('Removing autocomplete=off from the following elements:');
+    console.group(
+        'Removing autocomplete=off from the following %d element(s):',
+        elements.length);
     Array.prototype.forEach.call(elements, function(e) {
       console.log(e);
       e.removeAttribute('autocomplete');
     });
-  console.log('%d total.', elements.length);
-  console.groupEnd();
+    console.groupEnd();
   }
 })();
